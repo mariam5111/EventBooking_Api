d0666c8e9e7304e50cd8056a6488d69dd4bcc138
mariam <mariamshahathamada@gmail.com>
2026-08-15 04:51:17 +0300

    added app.js for all routes in the project and Middlewares and Error Handler

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+
+
+const userRouter = require("./routes/user.routes");
+const eventRouter = require("./routes/event.routes");
+const bookingRouter = require("./routes/booking.routes");
+
+
+const errorHandler = require("./middleware/errorHandler");
+const AppError = require("./utils/appError");
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+
+app.use("/api/users", userRouter);
+app.use("/api/events", eventRouter);
+app.use("/api/bookings", bookingRouter);
+
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+
+app.use(errorHandler);
+
+module.exports = app;
